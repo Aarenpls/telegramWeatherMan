3ab8be46ffedc125da4e6c56ab2cf71f141f9b39
Aaron Choo <choozy.aaron@gmail.com>
2019-03-29 15:05:10 +0800

    Users can now send location to get nearest station

--- telegram.py
+++ telegram.py
@@ -15,7 +15,8 @@ def getTemp(location):
         data = json.loads(url.read().decode())
     return data
 
-def findIndex(index, location, stations):
+def findIndex(location, stations):
+    index = 0
     for x in stations:
         if stations[index]["name"] == location:
             return index
@@ -24,7 +25,7 @@ def findIndex(index, location, stations):
             continue
 
 def printTemp(data, location, stations):
-    indexForData = findIndex(0, location, stations)
+    indexForData = findIndex(location, stations)
     try:
         tempToPrint = data["items"][0]["readings"][indexForData]["value"]
         toPrint = ("Current temperature: " + str(tempToPrint) + chr(176) + "C")
@@ -85,17 +86,81 @@ def getPSI(area):
         toPrint += " (This is bad...)"
     return toPrint
 
-@bot.message_handler(commands = ['start'])
+def getNearestStation(userLatitude, userLongitude):
+    utc_now = datetime.utcnow()
+    sg_now = utc_now + timedelta(hours=8)
+    currentDateTime = sg_now.strftime("%Y") + "-" + sg_now.strftime("%m") + "-" + sg_now.strftime("%d") + "T" + sg_now.strftime("%H") + "%3A" + sg_now.strftime("%M") + "%3A" + sg_now.strftime("%S")
+    currentDateTimeURL = "https://api.data.gov.sg/v1/environment/air-temperature?date_time=" + currentDateTime
+
+    with urllib.request.urlopen(currentDateTimeURL) as url:
+        data = json.loads(url.read().decode())
+
+    stations = data["metadata"]["stations"]
+
+    nearestStation = "dummy"
+    nearestDistance = 1000
+    index = 0
+    for x in stations:
+        stationLatitude = stations[index]["location"]["latitude"]
+        stationLongitude = stations[index]["location"]["longitude"]
+        latToCompare = abs(stationLatitude - userLatitude)
+        longToCompare = abs(stationLongitude - userLongitude)
+        distance = latToCompare + longToCompare
+        if distance < nearestDistance:
+            nearestDistance = distance
+            nearestStation = stations[index]["name"]
+        index = index + 1
+
+    return nearestStation
+
+def getNearestArea(location):
+    if location == "Ang Mo Kio Avenue 5":
+        return "central"
+    elif location == "Banyan Road":
+        return "west"
+    elif location == "Clementi Road":
+        return "west"
+    elif location == "East Coast Parkway":
+        return "east"
+    elif location == "Kim Chuan Road":
+        return "east"
+    elif location == "Marina Gardens Drive":
+        return "south"
+    elif location == "Nanyang Avenue":
+        return "west"
+    elif location == "Old Choa Chu Kang Road":
+        return "west"
+    elif location == "Pulau Ubin":
+        return "east"
+    elif location == "Scotts Road":
+        return "central"
+    elif location == "Sentosa":
+        return "south"
+    elif location == "Tuas South Avenue 3":
+        return "west"
+    elif location == "Upper Changi Road North":
+        return "east"
+    elif location == "West Coast Highway":
+        return "west"
+    elif location == "Woodlands Avenue 9":
+        return "north"
+    elif location == "Woodlands Road":
+        return "north"
+    else:
+        return "central"
+
+@bot.message_handler(commands = ['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Harlow, how are you?")
+    msg = "This is a bot that tells the current weather." + "\n" + "Type /<AREA> to get that location's current weather." + " \U0001F60A \U0001F61D"
+    bot.reply_to(message, msg)
 
 @bot.message_handler(commands = ['angmokio'])
 def send_welcome(message):
     location = "Ang Mo Kio Avenue 5"
     data = getTemp(location)
     stations = data["metadata"]["stations"]
-    area = "central"
-    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getForecast(area) + "\n" + getPSI(area)
+    area = getNearestArea(location)
+    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getPSI(area) + "\n" + getForecast(area)
     bot.reply_to(message, toPrint)
 
 @bot.message_handler(commands = ['banyan'])
@@ -103,8 +168,8 @@ def send_welcome(message):
     location = "Banyan Road"
     data = getTemp(location)
     stations = data["metadata"]["stations"]
-    area = "west"
-    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getForecast(area) + "\n" + getPSI(area)
+    area = getNearestArea(location)
+    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getPSI(area) + "\n" + getForecast(area)
     bot.reply_to(message, toPrint)
 
 @bot.message_handler(commands = ['clementi'])
@@ -112,8 +177,8 @@ def send_welcome(message):
     location = "Clementi Road"
     data = getTemp(location)
     stations = data["metadata"]["stations"]
-    area = "west"
-    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getForecast(area) + "\n" + getPSI(area)
+    area = getNearestArea(location)
+    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getPSI(area) + "\n" + getForecast(area)
     bot.reply_to(message, toPrint)
 
 @bot.message_handler(commands = ['eastcoast'])
@@ -121,8 +186,8 @@ def send_welcome(message):
     location = "East Coast Parkway"
     data = getTemp(location)
     stations = data["metadata"]["stations"]
-    area = "east"
-    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getForecast(area) + "\n" + getPSI(area)
+    area = getNearestArea(location)
+    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getPSI(area) + "\n" + getForecast(area)
     bot.reply_to(message, toPrint)
 
 @bot.message_handler(commands = ['kimchuan'])
@@ -130,8 +195,8 @@ def send_welcome(message):
     location = "Kim Chuan Road"
     data = getTemp(location)
     stations = data["metadata"]["stations"]
-    area = "east"
-    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getForecast(area) + "\n" + getPSI(area)
+    area = getNearestArea(location)
+    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getPSI(area) + "\n" + getForecast(area)
     bot.reply_to(message, toPrint)
 
 @bot.message_handler(commands = ['marinagardens'])
@@ -139,8 +204,8 @@ def send_welcome(message):
     location = "Marina Gardens Drive"
     data = getTemp(location)
     stations = data["metadata"]["stations"]
-    area = "south"
-    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getForecast(area) + "\n" + getPSI(area)
+    area = getNearestArea(location)
+    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getPSI(area) + "\n" + getForecast(area)
     bot.reply_to(message, toPrint)
 
 @bot.message_handler(commands = ['nanyang'])
@@ -148,8 +213,8 @@ def send_welcome(message):
     location = "Nanyang Avenue"
     data = getTemp(location)
     stations = data["metadata"]["stations"]
-    area = "west"
-    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getForecast(area) + "\n" + getPSI(area)
+    area = getNearestArea(location)
+    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getPSI(area) + "\n" + getForecast(area)
     bot.reply_to(message, toPrint)
 
 @bot.message_handler(commands = ['choachukang'])
@@ -157,8 +222,8 @@ def send_welcome(message):
     location = "Old Choa Chu Kang Road"
     data = getTemp(location)
     stations = data["metadata"]["stations"]
-    area = "west"
-    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getForecast(area) + "\n" + getPSI(area)
+    area = getNearestArea(location)
+    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getPSI(area) + "\n" + getForecast(area)
     bot.reply_to(message, toPrint)
 
 @bot.message_handler(commands = ['pulauubin'])
@@ -166,8 +231,8 @@ def send_welcome(message):
     location = "Pulau Ubin"
     data = getTemp(location)
     stations = data["metadata"]["stations"]
-    area = "east"
-    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getForecast(area) + "\n" + getPSI(area)
+    area = getNearestArea(location)
+    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getPSI(area) + "\n" + getForecast(area)
     bot.reply_to(message, toPrint)
 
 @bot.message_handler(commands = ['scotts'])
@@ -175,8 +240,8 @@ def send_welcome(message):
     location = "Scotts Road"
     data = getTemp(location)
     stations = data["metadata"]["stations"]
-    area = "central"
-    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getForecast(area) + "\n" + getPSI(area)
+    area = getNearestArea(location)
+    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getPSI(area) + "\n" + getForecast(area)
     bot.reply_to(message, toPrint)
 
 @bot.message_handler(commands = ['sentosa'])
@@ -184,8 +249,8 @@ def send_welcome(message):
     location = "Sentosa"
     data = getTemp(location)
     stations = data["metadata"]["stations"]
-    area = "south"
-    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV()+ "\n" + getForecast(area) + "\n" + getPSI(area)
+    area = getNearestArea(location)
+    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getPSI(area) + "\n" + getForecast(area)
     bot.reply_to(message, toPrint)
 
 @bot.message_handler(commands = ['tuas'])
@@ -193,8 +258,8 @@ def send_welcome(message):
     location = "Tuas South Avenue 3"
     data = getTemp(location)
     stations = data["metadata"]["stations"]
-    area = "west"
-    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getForecast(area) + "\n" + getPSI(area)
+    area = getNearestArea(location)
+    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getPSI(area) + "\n" + getForecast(area)
     bot.reply_to(message, toPrint)
 
 @bot.message_handler(commands = ['changi'])
@@ -202,8 +267,8 @@ def send_welcome(message):
     location = "Upper Changi Road North"
     data = getTemp(location)
     stations = data["metadata"]["stations"]
-    area = "east"
-    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getForecast(area) + "\n" + getPSI(area)
+    area = getNearestArea(location)
+    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getPSI(area) + "\n" + getForecast(area)
     bot.reply_to(message, toPrint)
 
 @bot.message_handler(commands = ['westcoast'])
@@ -211,8 +276,8 @@ def send_welcome(message):
     location = "West Coast Highway"
     data = getTemp(location)
     stations = data["metadata"]["stations"]
-    area = "west"
-    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getForecast(area) + "\n" + getPSI(area)
+    area = getNearestArea(location)
+    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getPSI(area) + "\n" + getForecast(area)
     bot.reply_to(message, toPrint)
 
 @bot.message_handler(commands = ['woodlandsave9'])
@@ -220,8 +285,8 @@ def send_welcome(message):
     location = "Woodlands Avenue 9"
     data = getTemp(location)
     stations = data["metadata"]["stations"]
-    area = "north"
-    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getForecast(area) + "\n" + getPSI(area)
+    area = getNearestArea(location)
+    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getPSI(area) + "\n" + getForecast(area)
     bot.reply_to(message, toPrint)
 
 @bot.message_handler(commands = ['woodlandsroad'])
@@ -229,8 +294,20 @@ def send_welcome(message):
     location = "Woodlands Road"
     data = getTemp(location)
     stations = data["metadata"]["stations"]
-    area = "north"
-    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getForecast(area) + "\n" + getPSI(area)
+    area = getNearestArea(location)
+    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getPSI(area) + "\n" + getForecast(area)
+    bot.reply_to(message, toPrint)
+
+@bot.message_handler(content_types=['location'])
+def handle_location(message):
+    userLatitude = message.location.latitude
+    userLongitude = message.location.longitude
+    print(userLatitude, userLongitude)
+    location = getNearestStation(userLatitude, userLongitude)
+    data = getTemp(location)
+    stations = data["metadata"]["stations"]
+    area = getNearestArea(location)
+    toPrint = location + "\n" + printTemp(data, location, stations) + "\n" + getUV() + "\n" + getPSI(area) + "\n" + getForecast(area)
     bot.reply_to(message, toPrint)
 
 while True:
